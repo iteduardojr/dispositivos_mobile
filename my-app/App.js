0f0ccc4172b034eeb61617238fd2cc6c706009e0
iteduardojr <it.eduardojr@gamil.com>
2023-09-06 21:14:03 -0300

    Profile

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import BiscoitoSorte from './components/semana05/BiscoitoSorte';
-// import BiscoitoSorte from './BiscoitoSorte';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Profile from './components/semana05/Profile'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <BiscoitoSorte/>
+      <Profile 
+      imgUri='https://www.fakepersongenerator.com/Face/female/female1022545970764.jpg'
+      genero='Feminino'
+      nome='Ana Anabel Analia'
+      email='abracadabra@gmail.com'
+      telefone='33443-324234-32432'
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-});
+  }
+})
